212b7ec2ed36415bc1faa6ad974cf2991c4b443d
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-05-25 21:56:46 +0300

    CHORE: padding and stuff ⚙️

--- landing/src/components/Landing/Mobile.tsx
+++ landing/src/components/Landing/Mobile.tsx
@@ -11,12 +11,12 @@ export default function Mobile() {
           The Coolest Cloud Music Player!
         </h2>
       </div>
-      <div className="absolute bottom-[40px] left-[50%] translate-x-[-50%]">
+      <div className="absolute mt-[10%] left-[50%] translate-x-[-50%]">
         <button
           className="bg-dark-secondary text-dark-neutral rounded-[20px] p-[13px] w-[228px] h-[54px] flex justify-between items-center"
           onClick={() => navigate("/intro")}
         >
-          <span className="text-[17px]">Discover the Beat</span>{" "}
+          <span className="text-[17px] font-[600]">Discover the Beat</span>{" "}
           <img className="inline" src="/headset-icon.png" />
         </button>
       </div>
--- landing/src/components/Signup/Mobile.tsx
+++ landing/src/components/Signup/Mobile.tsx
@@ -34,74 +34,80 @@ export default function Mobile() {
 
   return (
     <div className="w-[100vw] h-[100vh] bg-dark-primary text-dark-secondary">
-      <div className="flex justify-between">
+      <div className="flex justify-between w-[100vw]">
         <h1 className="text-white text-[28px] pt-[32px] pl-[30px]">
           Unlock a world of YOUR music with us!
         </h1>
-        <img className="mr-[-120px] w-[248px] h-[218px]" src="/cassette.png" />
+        <img
+          className="mr-[-120px] mt-[-20px] w-[248px] h-[218px]"
+          src="/cassette.png"
+        />
       </div>
-      <form onSubmit={handleSignup} className="pt-[0px] pl-[30px]">
-        <input
-          className="block bg-dark-primary text-dark-secondary border-[2px] border-dark-accent rounded-[20px] p-[16px] h-[56px] w-[330px]"
-          type="text"
-          placeholder="Full Name"
-          required
-          value={user.fullName}
-          onChange={(e) => {
-            user.fullName = e.target.value;
-            setUser({ ...user });
-          }}
-        />
-        <input
-          className="block bg-dark-primary text-dark-secondary border-[2px] border-dark-accent rounded-[20px] p-[16px] h-[56px] w-[330px] mt-[20px]"
-          type="email"
-          placeholder="Email"
-          required
-          value={user.email}
-          onChange={(e) => {
-            user.email = e.target.value;
-            setUser({ ...user });
-          }}
-        />
-        <input
-          className="block bg-dark-primary text-dark-secondary border-[2px] border-dark-accent rounded-[20px] p-[16px] h-[56px] w-[330px] mt-[20px]"
-          type="password"
-          minLength={8}
-          placeholder="Password"
-          required
-          value={user.password}
-          onChange={(e) => {
-            user.password = e.target.value;
-            setUser({ ...user });
-          }}
-        />
-        <input
-          className="block bg-dark-primary text-dark-secondary border-[2px] border-dark-accent rounded-[20px] p-[16px] h-[56px] w-[330px] mt-[20px]"
-          type="password"
-          minLength={8}
-          placeholder="Confirm Password"
-          required
-          value={confirmPassword}
-          onChange={(e) => {
-            setConfirmPassword(e.target.value);
-          }}
-        />
-        <input
-          type="submit"
-          className="bg-dark-accent text-dark-neutral w-[330px] h-[48px] rounded-[20px] mt-[25px] text-[24px] cursor-pointer"
-          value="Sign up"
-        />
-      </form>
 
-      <h3 className="w-full text-center mt-[25px]">
-        Already have an account? Log in{" "}
-        <a
-          href="https://apollo-music.app/sign-in"
-          className="underline hover:opacity-[0.9]"
-        >
-          here
-        </a>{" "}
-      </h3>
+      <div className="absolute left-[50%] translate-x-[-50%]">
+        <form onSubmit={handleSignup} className="pt-[0px]">
+          <input
+            className="block bg-dark-primary text-dark-secondary border-[2px] border-dark-accent rounded-[20px] p-[16px] h-[56px] w-[330px]"
+            type="text"
+            placeholder="Full Name"
+            required
+            value={user.fullName}
+            onChange={(e) => {
+              user.fullName = e.target.value;
+              setUser({ ...user });
+            }}
+          />
+          <input
+            className="block bg-dark-primary text-dark-secondary border-[2px] border-dark-accent rounded-[20px] p-[16px] h-[56px] w-[330px] mt-[20px]"
+            type="email"
+            placeholder="Email"
+            required
+            value={user.email}
+            onChange={(e) => {
+              user.email = e.target.value;
+              setUser({ ...user });
+            }}
+          />
+          <input
+            className="block bg-dark-primary text-dark-secondary border-[2px] border-dark-accent rounded-[20px] p-[16px] h-[56px] w-[330px] mt-[20px]"
+            type="password"
+            minLength={8}
+            placeholder="Password"
+            required
+            value={user.password}
+            onChange={(e) => {
+              user.password = e.target.value;
+              setUser({ ...user });
+            }}
+          />
+          <input
+            className="block bg-dark-primary text-dark-secondary border-[2px] border-dark-accent rounded-[20px] p-[16px] h-[56px] w-[330px] mt-[20px]"
+            type="password"
+            minLength={8}
+            placeholder="Confirm Password"
+            required
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+          />
+          <input
+            type="submit"
+            className="bg-dark-accent text-dark-neutral w-[330px] h-[48px] rounded-[20px] mt-[25px] text-[24px] cursor-pointer"
+            value="Sign up"
+          />
+        </form>
+
+        <h3 className="w-full text-center mt-[35px]">
+          Already have an account? Log in{" "}
+          <a
+            href="https://apollo-music.app/sign-in"
+            className="underline hover:opacity-[0.9]"
+          >
+            here
+          </a>{" "}
+        </h3>
+      </div>
     </div>
   );
 }
